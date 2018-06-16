71e5dbef55516a0556811b1b7501b6f4dfa7de62
data-pup <kevinmartinucr@gmail.com>
2018-06-16 11:28:43 -0400

    Added a call to fetch the .debug_types section of the file.

--- parser/object_parse.rs
+++ parser/object_parse.rs
@@ -50,6 +50,7 @@ impl<'input> Parse<'input> for object::File<'input> {
         let debug_ranges: gimli::DebugRanges<_> = load_section(&arena, self, endian);
         let debug_rnglists: gimli::DebugRngLists<_> = load_section(&arena, self, endian);
         let debug_str: gimli::DebugStr<_> = load_section(&arena, self, endian);
+        let _debug_types: gimli::DebugTypes<_> = load_section(&arena, self, endian);
 
         let rnglists = &gimli::RangeLists::new(debug_ranges, debug_rnglists)?;
 
