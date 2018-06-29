819ab5daebe470ce146f5c951e94cbb56cd5fc29
data-pup <kevinmartinucr@gmail.com>
2018-06-29 17:18:01 -0400

    Added some more detail to the location attributes struct.

--- parser/object_parse/die_parse.rs
+++ parser/object_parse/die_parse.rs
@@ -8,38 +8,57 @@ use super::die_ir_item_size::subroutine_size;
 use super::die_is_edge::is_edge;
 use super::Parse;
 
-type FallibleOption<T> = Result<Option<T>, traits::Error>;
-
-struct _LocationAttributes<R: gimli::Reader> {
-    dw_at_low_pc: FallibleOption<gimli::AttributeValue<R>>,
-    dw_at_high_pc: FallibleOption<gimli::AttributeValue<R>>,
-    dw_at_entry_pc: FallibleOption<gimli::AttributeValue<R>>,
-    dw_at_ranges: FallibleOption<gimli::AttributeValue<R>>,
+struct _DieLocationAttributes<R: gimli::Reader> {
+    dw_tag: gimli::DwTag,
+    dw_at_low_pc: Option<gimli::AttributeValue<R>>,
+    dw_at_high_pc: Option<gimli::AttributeValue<R>>,
+    dw_at_entry_pc: Option<gimli::AttributeValue<R>>,
+    dw_at_ranges: Option<gimli::AttributeValue<R>>,
 }
 
-impl<R: gimli::Reader> _LocationAttributes<R> {
+impl<R: gimli::Reader> _DieLocationAttributes<R> {
     /// Try to create a new location attributes instance using the given
     /// debugging information entry (DIE). Reading these attributes may fail,
     /// so this will return a Result rather than a plain `Self`.
-    fn try_from(
-        die: &gimli::DebuggingInformationEntry<R, R::Offset>,
+    fn _try_from(
+        _die: &gimli::DebuggingInformationEntry<R, R::Offset>,
     ) -> Result<Self, traits::Error> {
         unimplemented!();
     }
 
-    /// Return a boolean specifying whether or not this DIE represents the
-    /// definition of a subroutine. DIEs without any location attributes
+    /// Return a boolean value specifying whether or not this DIE represents
+    /// a subprogram.
+    fn _is_subprogram(&self) -> bool {
+        match self.dw_tag {
+            gimli::DW_TAG_subprogram => true,
+            _ => false,
+        }
+    }
+
+    /// Return a boolean value specifying whether or not this DIE represents
+    /// the definition of a subroutine. DIEs without any location attributes
     /// represent a declaration.
-    fn is_declaration(&self) -> Result<bool, traits::Error> {
-        unimplemented!();
+    fn _is_definition(&self) -> bool {
+        self.dw_at_low_pc.is_some() && self.dw_at_entry_pc.is_some()
     }
 
-    fn base_addr(&self) -> FallibleOption<gimli::AttributeValue<R>> {
-        unimplemented!();
+    /// Return a boolean value specifying whether or not this DIE occupies
+    /// a contiguous range of machine code addresses.
+    fn _is_contiguous(&self) -> bool {
+        self.dw_at_low_pc.is_some() && self.dw_at_high_pc.is_some()
     }
 
-    fn is_contiguous() -> FallibleOption<gimli::AttributeValue<R>> {
+    /// Return the base address, which will be the value of `DW_AT_low_pc`,
+    /// or `DW_AT_entry_pc` if the former attribute does not exist.
+    fn _base_addr(&self) -> Option<gimli::AttributeValue<R>> {
         unimplemented!();
+        // if self.dw_at_low_pc.is_some() {
+        //     self.dw_at_low_pc
+        // } else if self.dw_at_entry_pc.is_some() {
+        //     self.dw_at_entry_pc
+        // } else {
+        //     None
+        // }
     }
 }
 
