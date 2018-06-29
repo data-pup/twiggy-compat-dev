739d5d180c3c83484c99ad773a4153cc6fdf7e99
data-pup <kevinmartinucr@gmail.com>
2018-06-29 19:08:12 -0400

    Moved the location attribute into a new file.

--- parser/object_parse/die_parse/mod.rs
+++ parser/object_parse/die_parse/mod.rs
@@ -6,6 +6,7 @@ mod item_kind;
 mod item_name;
 mod item_size;
 mod is_edge;
+mod location_attrs;
 
 use self::item_kind::item_kind;
 use self::item_name::item_name;
@@ -13,60 +14,6 @@ use self::item_size::subroutine_size;
 use self::is_edge::is_edge;
 
 use super::Parse;
-
-struct _DieLocationAttributes<R: gimli::Reader> {
-    dw_tag: gimli::DwTag,
-    dw_at_low_pc: Option<gimli::AttributeValue<R>>,
-    dw_at_high_pc: Option<gimli::AttributeValue<R>>,
-    dw_at_entry_pc: Option<gimli::AttributeValue<R>>,
-    dw_at_ranges: Option<gimli::AttributeValue<R>>,
-}
-
-impl<R: gimli::Reader> _DieLocationAttributes<R> {
-    /// Try to create a new location attributes instance using the given
-    /// debugging information entry (DIE). Reading these attributes may fail,
-    /// so this will return a Result rather than a plain `Self`.
-    fn _try_from(
-        _die: &gimli::DebuggingInformationEntry<R, R::Offset>,
-    ) -> Result<Self, traits::Error> {
-        unimplemented!();
-    }
-
-    /// Return a boolean value specifying whether or not this DIE represents
-    /// a subprogram.
-    fn _is_subprogram(&self) -> bool {
-        match self.dw_tag {
-            gimli::DW_TAG_subprogram => true,
-            _ => false,
-        }
-    }
-
-    /// Return a boolean value specifying whether or not this DIE represents
-    /// the definition of a subroutine. DIEs without any location attributes
-    /// represent a declaration.
-    fn _is_definition(&self) -> bool {
-        self.dw_at_low_pc.is_some() && self.dw_at_entry_pc.is_some()
-    }
-
-    /// Return a boolean value specifying whether or not this DIE occupies
-    /// a contiguous range of machine code addresses.
-    fn _is_contiguous(&self) -> bool {
-        self.dw_at_low_pc.is_some() && self.dw_at_high_pc.is_some()
-    }
-
-    /// Return the base address, which will be the value of `DW_AT_low_pc`,
-    /// or `DW_AT_entry_pc` if the former attribute does not exist.
-    fn _base_addr(&self) -> Option<gimli::AttributeValue<R>> {
-        unimplemented!();
-        // if self.dw_at_low_pc.is_some() {
-        //     self.dw_at_low_pc
-        // } else if self.dw_at_entry_pc.is_some() {
-        //     self.dw_at_entry_pc
-        // } else {
-        //     None
-        // }
-    }
-}
 
 pub struct DIEItemsExtra<'unit, R>
 where
