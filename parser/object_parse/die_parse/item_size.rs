41d6c7edfd84ed1fd68040ee0d8ce0e7cc0f65a8
data-pup <kevinmartinucr@gmail.com>
2018-06-29 21:15:54 -0400

    Further work cleaning up size calculation for subroutine DIES.

--- parser/object_parse/die_parse/item_size.rs
+++ parser/object_parse/die_parse/item_size.rs
@@ -1,6 +1,8 @@
 use gimli;
 use traits;
 
+use super::DieLocationAttributes;
+
 /// Find the size of an entity that has a machine code address, or a range of
 /// machine code addresses. This includes compilation units, module
 /// initialization, subroutines, lexical blocks, try/catch blocks (see Section
@@ -9,7 +11,7 @@ use traits;
 /// For more information about this, refer to Chapter 2.17 'Code Addresses,
 /// Ranges, and Base Addresses' (pg. 51) in the DWARF5 specification.
 pub fn subroutine_size<R>(
-    die: &gimli::DebuggingInformationEntry<R, R::Offset>,
+    location_attrs: DieLocationAttributes<R>,
     addr_size: u8,
     version: u16,
     rnglists: &gimli::RangeLists<R>,
@@ -17,19 +19,19 @@ pub fn subroutine_size<R>(
 where
     R: gimli::Reader,
 {
-    let low_pc_value: Option<u64> = get_low_pc_value(die)?;
+    let low_pc_value: Option<u64> = location_attrs.get_low_pc_value()?;
 
     if let Some(base_addr) = low_pc_value {
-        let high_pc_val = die.attr_value(gimli::DW_AT_high_pc)?;
+        let high_pc_val = location_attrs.get_high_pc();
         get_contiguous_item_size(base_addr, high_pc_val, addr_size)
     } else {
-        let base_addr = die.attr_value(gimli::DW_AT_entry_pc)?;
+        let base_addr = location_attrs.get_entry_pc();
         if base_addr.is_none() {
             // FIXUP: A subroutine entry representing a subroutine declaration that
             // is not also a definition does not have code address or range attributes.
             return Ok(0);
         }
-        let ranges_attr = die.attr_value(gimli::DW_AT_ranges)?.unwrap(); // FIXUP.
+        let ranges_attr = location_attrs.get_ranges().unwrap(); // FIXUP
         get_ranges_item_size(
             ranges_attr,
             base_addr.unwrap(),
@@ -40,19 +42,6 @@ where
     }
 }
 
-fn get_low_pc_value<R>(
-    die: &gimli::DebuggingInformationEntry<R, R::Offset>,
-) -> Result<Option<u64>, traits::Error>
-where
-    R: gimli::Reader,
-{
-    match die.attr_value(gimli::DW_AT_low_pc)? {
-        Some(gimli::AttributeValue::Addr(address)) => Ok(Some(address)),
-        Some(_) => Err(traits::Error::with_msg("Unexpected DW_AT_low_pc value")),
-        None => Ok(None),
-    }
-}
-
 /// FIXUP FIXUP FIXUP: UPDATE THESE COMMENTS.
 /// Find the value of the `DW_AT_low_pc` for a DIE representing an entity with
 /// a contiguous range of machine code addresses. If there is not a
@@ -60,7 +49,7 @@ where
 /// `DW_AT_ranges` should be used instead.
 fn get_contiguous_item_size<R>(
     low_pc_val: u64,
-    high_pc_val: Option<gimli::AttributeValue<R>>,
+    high_pc_val: Option<&gimli::AttributeValue<R>>,
     addr_size: u8,
 ) -> Result<u64, traits::Error>
 where
@@ -69,7 +58,7 @@ where
     if let Some(high_pc_attr) = high_pc_val {
         match high_pc_attr {
             gimli::AttributeValue::Addr(end_addr) => Ok(end_addr - low_pc_val),
-            gimli::AttributeValue::Udata(offset) => Ok(offset),
+            gimli::AttributeValue::Udata(offset) => Ok(*offset),
             _ => Err(traits::Error::with_msg(
                 "Unexpected DW_AT_high_pc attribute value",
             )),
@@ -83,8 +72,8 @@ where
 ///
 /// Get the size of an entity that occupies non-contiguous address ranges.
 fn get_ranges_item_size<R>(
-    _ranges_attr: gimli::AttributeValue<R>,
-    _base_addr: gimli::AttributeValue<R>,
+    _ranges_attr: &gimli::AttributeValue<R>,
+    _base_addr: &gimli::AttributeValue<R>,
     _addr_size: u8,
     _version: u16,
     _rnglists: &gimli::RangeLists<R>,
