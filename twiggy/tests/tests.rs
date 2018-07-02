e9b23ee3d51dfde8aa37060464d23c406e3ad749
data-pup <kevinmartinucr@gmail.com>
2018-07-02 15:10:15 -0400

    Formatting, oops.

--- twiggy/tests/tests.rs
+++ twiggy/tests/tests.rs
@@ -393,8 +393,10 @@ test!(
     "json"
 );
 
-test!(elf_top_25_hello_world_rs, "top",
-"-n",
-"25",
-"./fixtures/hello_elf"
+test!(
+    elf_top_25_hello_world_rs,
+    "top",
+    "-n",
+    "25",
+    "./fixtures/hello_elf"
 );
